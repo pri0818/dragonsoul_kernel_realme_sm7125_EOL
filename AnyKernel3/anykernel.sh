51ef5c6f74a9b8deaee3f16c17e71c00f5550474
pri0818 <priyanshusinghal0818@gmail.com>
2022-07-18 11:39:13 +0000

    DragonSouL: Uprev to DragonSouL SaphirA Z1x
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Let the Dragon be in the sky
+kernel.string=The fire brething Father of Dragons
 do.devicecheck=1
 do.modules=0
 do.systemless=0
